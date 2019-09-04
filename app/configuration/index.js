bafb52a370562bcadb0fab39991677a55f3e0b7b
tohas1986 <tohas1986>
2019-09-04 14:56:05 +0300

    First commit

---
+++ app/configuration/index.js
@@ -0,0 +1,57 @@
+/**
+ * A module for the configuration
+ *
+ * @module app/configuration/index
+ * @exports app/configuration/index
+ */
+
+window.angular && (function(angular) {
+  'use strict';
+
+  angular
+      .module('app.configuration', ['ngRoute', 'app.common.services'])
+      // Route configuration
+      .config([
+        '$routeProvider',
+        function($routeProvider) {
+          $routeProvider
+              .when('/configuration/network', {
+                'template': require('./controllers/network-controller.html'),
+                'controller': 'networkController',
+                authenticated: true
+              })
+              .when('/configuration/date-time', {
+                'template': require('./controllers/date-time-controller.html'),
+                'controller': 'dateTimeController',
+                authenticated: true
+              })
+              .when('/configuration', {
+                'template': require('./controllers/network-controller.html'),
+                'controller': 'networkController',
+                authenticated: true
+              })
+              .when('/configuration/snmp', {
+                'template': require('./controllers/snmp-controller.html'),
+                'controller': 'snmpController',
+                authenticated: true
+              })
+              .when('/configuration/virtual-media', {
+                'template':
+                    require('./controllers/virtual-media-controller.html'),
+                'controller': 'virtualMediaController',
+                authenticated: true
+              })
+              .when('/configuration/certificate', {
+                'template':
+                    require('./controllers/certificate-controller.html'),
+                'controller': 'certificateController',
+                authenticated: true
+              })
+              .when('/configuration/firmware', {
+                'template': require('./controllers/firmware-controller.html'),
+                'controller': 'firmwareController',
+                authenticated: true
+              });
+        }
+      ]);
+})(window.angular);
